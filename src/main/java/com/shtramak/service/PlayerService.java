0257aa8ca8943ee35620b599e53cdba3df105b08
shtramak <shtramak@gmail.com>
2018-12-14 16:47:09 +0200

    mod PlayerService and PlayerServiceImpl
    Add addPlayerToTeam method to PlayerService.
    Modify logic of removeById method, to provide removing player if he
    already exists in a team;
    implement addPlayerToTeam method with checking if player already exists
    in any team

--- src/main/java/com/shtramak/service/PlayerService.java
+++ src/main/java/com/shtramak/service/PlayerService.java
@@ -3,4 +3,5 @@ package com.shtramak.service;
 import com.shtramak.entity.Player;
 
 public interface PlayerService extends CrudService<Player> {
+    public void addPlayerToTeam(Long playerId, Long teamId);
 }
--- src/main/java/com/shtramak/service/PlayerServiceImpl.java
+++ src/main/java/com/shtramak/service/PlayerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.shtramak.service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import com.shtramak.entity.Player;
+import com.shtramak.entity.Team;
 import com.shtramak.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +38,30 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public void removeById(Long id) {
-        repository.deleteById(id);
+        Optional<Player> optPlayer = getById(id);
+        Player player = optPlayer.orElseThrow();
+        Team team = player.getTeam();
+        if (team != null) {
+            if (Objects.equals(player.getId(), team.getCaptainId())) {
+                team.setCaptainId(null);
+            }
+            team.removePlayer(player);
+        } else {
+            repository.deleteById(id);
+        }
+    }
+
+    @Override
+    public void addPlayerToTeam(Long playerId, Long teamId) {
+        Optional<Player> optPlayer = getById(playerId);
+        Player player = optPlayer.orElseThrow();
+        Team playerTeam = player.getTeam();
+        if (playerTeam != null) {
+            throw new IllegalArgumentException("Player already plays for a team with id=" + playerTeam.getId());
+        }
+
+        repository.addPlayerToTeam(playerId, teamId);
     }
 }
+
+//todo what about removeById?
