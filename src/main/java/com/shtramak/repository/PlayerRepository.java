8b55bad4550e9ad3b42e80a224ff4c62a34ce4bf
shtramak <shtramak@gmail.com>
2018-12-14 16:43:16 +0200

    mod PlayerRepository. Add addPlayerToTeam method with @Modify

--- src/main/java/com/shtramak/repository/PlayerRepository.java
+++ src/main/java/com/shtramak/repository/PlayerRepository.java
@@ -2,6 +2,15 @@ package com.shtramak.repository;
 
 import com.shtramak.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    @Modifying
+    @Query("UPDATE Player p SET p.team.id=:teamId where p.id=:playerId")
+    void addPlayerToTeam(@Param("playerId") Long playerId, @Param("teamId") Long teamId);
 }
+
+//todo method doesn't work without @Modifying annotation... Why?
